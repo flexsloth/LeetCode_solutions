e46cf1070546ce687886e65ebe6f28661059c0c2
Om Gupta <123227494+flexsloth@users.noreply.github.com>
2023-07-16 22:56:25 +0530

    Time: 990 ms (11.11%), Space: 187.2 MB (11.11%) - LeetHub

---
+++ 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maximumBeauty(vector<int>& nums, int k) {
+        vector<int>maxdiff;
+        map<int,int>mp;
+        for(int i = 0 ; i < nums.size() ; i++){
+            mp[nums[i]-k]++;
+            mp[nums[i]+k+1]--;
+        }
+        int ret=0,tt=0;
+        for(auto x : mp){
+            tt += x.second;
+            ret = max(ret,tt);
+        }
+        return ret;
+    }
+};
