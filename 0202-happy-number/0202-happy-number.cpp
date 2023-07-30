5b9f92cea0d78b36fc4a82804156d8370cc11024
Om Gupta <123227494+flexsloth@users.noreply.github.com>
2023-07-30 16:34:18 +0530

    Time: 46 ms (7.05%), Space: 5.8 MB (99.00%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        int a = 10000;
+        while(a--){
+            if(n == 1) return true;
+            int a=n,gg=0;
+            while(n){
+                 gg+=pow(n%10,2);
+                n/=10;
+            }
+            n=gg;
+        }
+        return false;
+    }
+};
