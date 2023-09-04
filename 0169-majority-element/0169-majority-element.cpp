d377f1f278a640edb95dd06136753ceb967692b5
Om Gupta <123227494+flexsloth@users.noreply.github.com>
2023-09-04 22:33:45 +0530

    Time: 11 ms (89.42%), Space: 19.7 MB (6.47%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -3,11 +3,12 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         sort(nums.begin() , nums.end());
+        nums.push_back(-1);
         int n = nums.size();
         int gg=0,ans=0;
         int count=1;
         int i;
-        for( i = 0 ; i < nums.size()-1 ; i++){ //1 1 1 2 2 2 2 
+        for( i = 0 ; i < nums.size()-1 ; i++){ 
             if(nums[i] == nums[i+1]){
                 count++;
             }
@@ -19,7 +20,7 @@ public:
                 count=1;
             }
         }
-        if(count>gg) ans=nums[i];
+        // if(count>gg) ans=nums[i];
         return ans;
     }
 };
