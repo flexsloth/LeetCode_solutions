fc5feb2ffbbe511761c5110db5818bc47727c471
Om Gupta <123227494+flexsloth@users.noreply.github.com>
2023-09-07 23:50:24 +0530

    Time: 3 ms (77.81%), Space: 9.7 MB (58.33%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
-    int searchInsert(vector<int>& nums, int target) {
-        int gg;
-        int ans=-1;
-        for(int i = 0 ; i < nums.size() ; i++){
-            if(nums[i] == target) return i;
-            else if(target>=nums[i])    ans=i;
+    int searchInsert(vector<int>& nums, int target) { 
+     int n = nums.size();
+        for(int i = 0; i<n;i++)
+        {
+            if(nums[i]>=target)
+                return i;
         }
-         return (ans == -1)? 0 : ans+1;
-        // return ans+1;
-    }
+        return n;
+    }
 };
