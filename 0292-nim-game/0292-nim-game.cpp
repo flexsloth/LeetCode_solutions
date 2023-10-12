7eee03527ceda8946381002f98de38c6449b4d7a
Om Gupta <123227494+flexsloth@users.noreply.github.com>
2023-10-12 21:43:21 +0530

    Time: 2 ms (53.08%), Space: 6.5 MB (6.39%) - LeetHub

--- 0292-nim-game/0292-nim-game.cpp
+++ 0292-nim-game/0292-nim-game.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
     bool canWinNim(int n) {
-        if(n<4) return 1;
-        else if(n%4==0) return 0;
+        if(n%4==0) return 0;
         else return 1;
-        
     }
 };
