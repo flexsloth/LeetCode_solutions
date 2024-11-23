b1379e597a5aa7b4c51c8cf115816d662a494beb
Om Gupta <123227494+flexsloth@users.noreply.github.com>
2024-11-23 07:13:57 +0530

    Time: 0 ms (100%), Space: 8.8 MB (99.28%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,23 +1,12 @@
 class Solution {
 public:
-    vector<vector<int>>ans;
-    void helper(vector<int>& nums,vector<bool>&vis, vector<int>temp){
-        if(temp.size() == nums.size()){
-            ans.push_back(temp);
-        }
-        for(int i = 0 ; i < nums.size() ; i++){
-            if(vis[i]!=true){
-                temp.push_back(nums[i]);
-                vis[i] = true;
-                helper(nums,vis,temp);
-                vis[i] = false;
-                temp.pop_back();
-            }
-        }
-    }
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<bool>vis(nums.size() , false);
-        helper(nums,vis,{});
+        vector<vector<int>>ans;
+        sort(nums.begin() , nums.end());
+        ans.push_back(nums);
+        while(next_permutation(nums.begin() , nums.end())){
+            ans.push_back(nums);
+        }
         return ans;
     }
 };
