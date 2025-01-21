1ff115191b80290537d98725b9aeaa521d92f36d
Om Gupta <123227494+flexsloth@users.noreply.github.com>
2025-01-22 00:08:03 +0530

    Time: 0 ms (100%), Space: 9.4 MB (97.41%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int a=0,b=0;
+        for(int i = 0 ; i < nums.size() ; i++){
+            if(i&1){
+                a = max( a + nums[i] , b);
+            }
+            else{
+                b = max( b + nums[i] , a);
+            }
+        }
+        return max(a,b);
+    }
+};
