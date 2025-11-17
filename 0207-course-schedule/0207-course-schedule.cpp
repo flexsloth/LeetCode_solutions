c6ed21e069b15b4e5dcf126ed226fce5c3991884
Om Gupta <123227494+flexsloth@users.noreply.github.com>
2025-11-17 11:21:28 +0530

    Time: 4 ms (67.31%), Space: 21.2 MB (10.49%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
-    vector<vector<int>>graph;
-    vector<int>visited;
-    bool dfs(int i){
-        if(visited[i] == 1) return true;
-        if(visited[i] == 2) return false;
-        visited[i] = 1;
-        for(auto x : graph[i]){
-            if(dfs(x)){
-                return true;
+    bool canFinish(int n, vector<vector<int>>& pre) {
+        vector<vector<int>>adj(n);
+        for(auto i : pre){
+            adj[i[0]].push_back(i[1]);
+        }
+        vector<int>indegree(n);
+        for(auto i : pre){
+            indegree[i[1]]++;
+        }
+        queue<int>q;
+        for(int i = 0 ; i < n ; i++){
+            if(indegree[i] == 0) q.push(i);
+        }
+        vector<int>topo;
+        while(!q.empty()){
+            int gg = q.front();
+            q.pop();
+            topo.push_back(gg);
+            for(auto i : adj[gg]){
+                indegree[i]--;
+                if(indegree[i] == 0){
+                    q.push(i);
+                }
             }
         }
-        visited[i] = 2;
+        if(topo.size() == n) return true;
         return false;
-    }
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        graph.resize(numCourses);
-        visited.assign(numCourses, 0);
-        for(auto i : prerequisites){
-            graph[i[0]].push_back(i[1]);
-        }
-        for(int i = 0 ; i < numCourses ; i++){
-            if(!visited[i] && dfs(i)){
-                return false;
-            }
-        }
-        return true;
     }
 };
