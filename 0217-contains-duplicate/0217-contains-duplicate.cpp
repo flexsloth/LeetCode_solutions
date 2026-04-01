a880976698aa7bdda4bc452188a6ca46191f6f1f
Om Gupta <123227494+flexsloth@users.noreply.github.com>
2026-04-01 22:16:47 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin() , nums.end());
-        for(int i = 1 ; i < nums.size() ; i++){
-            if(nums[i] == nums[i-1]){
-                return true;
-            }
+        unordered_map<int,int>mp;
+        for(auto i : nums){
+            if(mp.find(i)!=mp.end()) return true;
+            mp[i]++;
         }
         return false;
     }
